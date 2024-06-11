6a14350047a40931658a1f8edd3b5eddde7411b3
JustinBrownSax <justinbrownsax@gmail.com>
2024-06-11 11:35:04 -0500

    Initial Commit

---
+++ Cs303 Assignment 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cs303 Assignment 1
+//
+//  Created by Justin Brown on 6/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
